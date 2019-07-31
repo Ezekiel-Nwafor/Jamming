9e304ac680ceb694f1fec101c1d16c462234671d
Ezekiel-Nwafor <29420639+Ezekiel-Nwafor@users.noreply.github.com>
2019-07-31 12:15:13 -0400

    organized components and began coding

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import SearchBar from '../SearchBar/SearchBar.js';
 import './App.css';
+import SearchResults from '../SearchResults/SearchResults.js';
+import Playlist from '../Playlist/Playlist.js';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span class="highlight">mmm</span>ing</h1>
-      <div class="App">
-        <!-- Add a SearchBar component -->
-        <div class="App-playlist">
-          <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component -->
-        </div>
-      </div>
-    </div>
-  );
+
+class App extends React.Component{
+
+  render(){
+    return (
+      <div>
+         <h1>Ja<span className="highlight">mmm</span>ing</h1>
+         <div className="App">
+           {/* Add a SearchBar component */}
+           <div className="App-playlist">
+             {/* Add a SearchResults component */}
+             {/* Add a Playlist component */}
+           </div>
+         </div>
+       </div>
+     );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-
-
-/* 
-**this is a template
-<div className="SearchBar">
-  <input placeholder="Enter A Song, Album, or Artist" />
-  <button claas="SearchButton">SEARCH</button>
-</div>
- */
